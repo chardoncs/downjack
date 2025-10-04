d6910a56e98fd2bfcc25331b98f832032f108dcb
Charles Dong <chardon_cs@proton.me>
2025-10-04 10:30:27 +0800

    Update command name

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/chardoncs/downjack/cmd/gitignore"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use: fmt.Sprintf("%s [commands] [flags] [values]", os.Args[0]),
+	Use: "downjack [commands] [flags] [values]",
 	Short: "Set up your gitignore and license files like a lumberjack",
 	Version: "0.1.0",
 }
