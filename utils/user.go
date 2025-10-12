d288f5075cf2e08296c7f22cd064eecc4bdbfe38
Charles Dong <chardon_cs@proton.me>
2025-10-12 16:40:22 +0800

    Remove redundant command exec

--- utils/user.go
+++ utils/user.go
@@ -5,9 +5,6 @@ import "os/exec"
 func GetUserName() (string, error) {
 	// Get from Git
 	cmd := exec.Command("git", "config", "user.name")
-	if err := cmd.Run(); err != nil {
-		return "", err
-	}
 
 	b, err := cmd.Output()
 	if err != nil {
