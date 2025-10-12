867fdbc38ff8c87d5f48c37abdc4e60bc42bfaaa
Charles Dong <chardon_cs@proton.me>
2025-10-12 10:08:18 +0800

    Update write license function

--- internal/licenses/write.go
+++ internal/licenses/write.go
@@ -1,5 +1,50 @@
 package licenses
 
+import (
+	"os"
+	"strings"
+	"text/template"
+)
+
 func WriteLicense(item MatchedItem, target string) error {
+	b, err := Root.ReadFile(item.Filename)
+	if err != nil {
+		return err
+	}
+
+	targetFile, err := os.OpenFile(
+		target,
+		os.O_WRONLY | os.O_CREATE | os.O_TRUNC,
+		0664,
+	)
+	if err != nil {
+		return err
+	}
+	defer targetFile.Close()
+
+	if isGoTemplate(item.Filename) {
+		tmpl, err := template.New(item.Id).Parse(string(b))
+		if err != nil {
+			return err
+		}
+
+		data, err := newLicenseInfo()
+		if err != nil {
+			return err
+		}
+
+		if err := tmpl.Execute(targetFile, data); err != nil {
+			return err
+		}
+	} else {
+		if _, err := targetFile.Write(b); err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func isGoTemplate(filename string) bool {
+	return strings.HasSuffix(filename, ".tmpl")
 }
