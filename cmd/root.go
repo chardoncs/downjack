0ccb270eb0530c930707d04d95fd4722ebaf1dac
Charles Dong <chardon_cs@proton.me>
2025-10-13 20:53:04 +0800

    Enable autocompletion generator

--- cmd/root.go
+++ cmd/root.go
@@ -11,9 +11,6 @@ var rootCmd = &cobra.Command{
 	Use: "downjack [command]",
 	Short: "Set up your gitignore and license files like using a lumberjack",
 	Version: "0.1.0",
-	CompletionOptions: cobra.CompletionOptions{
-		DisableDefaultCmd: true,
-	},
 	// Use custom error display instead
 	SilenceUsage: true,
 	SilenceErrors: true,
